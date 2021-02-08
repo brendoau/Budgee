3a9c153ab29df28ea0f1d729acf3f8ef49632ca5
Brendan Downes <brendoau@gmail.com>
2021-02-08 21:33:02 +0800

    added temp Clock component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Clock from './components/Clock'
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <Clock />
+        <Clock />
+        <Clock />
       </header>
     </div>
   );
