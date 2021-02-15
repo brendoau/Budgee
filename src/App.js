94fb29ade3e2029672ccfacb19745b3274caefb6
Brendan Downes <brendoau@gmail.com>
2021-02-15 22:53:34 +0800

    added first routes (super)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import Clock from './components/Clock'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Clock />
-        <Clock />
-        <Clock />
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/super">Superannuation</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/super">
+            <Super />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Super() {
+  return <h2>Superannuation</h2>;
 }
 
 export default App;
