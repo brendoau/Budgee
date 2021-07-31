5e5a915b2fe2a7a2f49ce727af9623ddf909a29d
Brendan Downes <brendoau@gmail.com>
2021-07-31 15:39:16 +0800

    added table

--- src/App.js
+++ src/App.js
@@ -2,29 +2,37 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Uploader from './components/Uploader';
 import Transactions from './components/Transactions';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Router>
+    <>
+      <Router>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Budgee</Navbar.Brand>
+            <Nav className="me-auto">
+              <Link class="nav-link" to="/uploader">Upload</Link>
+              <Link class="nav-link" to="/transactions">Transactions</Link>
+            </Nav>
+          </Container>
+        </Navbar>
 
-      <Link class="nav-link" to="/">Home</Link>
-      <Link class="nav-link" to="/uploader">Uploader</Link>
-      <Link class="nav-link" to="/transactions">Transactions</Link>
+        <Switch>
+          <Route path="/uploader">
+            <Uploader />
+          </Route>
+          <Route path="/transactions">
+            <Transactions />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
 
-      <Switch>
-        <Route path="/uploader">
-          <Uploader />
-        </Route>
-        <Route path="/transactions">
-          <Transactions />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-
-    </Router>
+      </Router>
+    </>
   );
 }
 
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import firebase from './Firebase/firebase';
 import Table from 'react-bootstrap/Table';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default function Transactions() {
     const [transactions, setTransactions] = useState([]);
@@ -27,36 +27,40 @@ export default function Transactions() {
     }
 
     return (
-        <Container>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </Table>
+        <Container className="mt-4">
+            <Row>
+                <Col>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td colSpan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
         </Container>
         // <TableContainer component={Paper}>
         //     <Table className={classes.table} aria-label="simple table">
